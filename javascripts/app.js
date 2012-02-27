7a4eceaffc75208ebe6781f67f66f5de98503dc7
Tad Thorley <phaedryx@gmail.com>
2012-02-26 23:12:27 -0700

    coffeescript text areas

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,22 @@
 (function() {
 
   $(document).ready(function() {
-    var htmlOptions;
+    var coffeescriptOptions, htmlOptions;
+    coffeescriptOptions = {
+      mode: 'text/x-coffeescript',
+      tabMode: 'indent',
+      tabSize: 2,
+      lineNumbers: true
+    };
     htmlOptions = {
       mode: 'text/html',
       tabMode: 'indent',
       tabSize: 2,
       lineNumbers: true
     };
+    $("textarea.coffeescript").each(function() {
+      return CodeMirror.fromTextArea($(this)[0], coffeescriptOptions);
+    });
     return $("textarea.html").each(function() {
       return CodeMirror.fromTextArea($(this)[0], htmlOptions);
     });
--- src/app.coffee
+++ src/app.coffee
@@ -1,8 +1,17 @@
 $(document).ready ->
+  coffeescriptOptions = {
+    mode: 'text/x-coffeescript'
+    tabMode: 'indent'
+    tabSize: 2
+    lineNumbers: true
+  }
   htmlOptions = {
     mode: 'text/html'
     tabMode: 'indent'
     tabSize: 2
     lineNumbers: true
   }
+  
+  $("textarea.coffeescript").each -> CodeMirror.fromTextArea($(this)[0], coffeescriptOptions)
+
   $("textarea.html").each -> CodeMirror.fromTextArea($(this)[0], htmlOptions)
