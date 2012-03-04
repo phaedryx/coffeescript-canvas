7607a6a79eaabf6abee81481137a33dd20bd0f14
Tad Thorley <phaedryx@gmail.com>
2012-03-03 23:41:43 -0700

    real-time code editing and an example

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,20 +1,45 @@
 (function() {
+  var coffeescriptOptions, compile, htmlOptions;
+
+  compile = function(editor, keyEvent) {
+    var alertDiv, javascript, source, _ref;
+    if (keyEvent.type !== "keyup") return;
+    if ((_ref = keyEvent.keyCode) === 16 || _ref === 37 || _ref === 38 || _ref === 39 || _ref === 40) {
+      return;
+    }
+    alertDiv = $(editor.getTextArea()).data('alert');
+    source = editor.getValue();
+    try {
+      javascript = CoffeeScript.compile(source, {
+        bare: true
+      });
+      eval(javascript);
+      return $(alertDiv).html("<i class='icon-ok'></i> Successfully Compiled");
+    } catch (error) {
+      return $(alertDiv).html("<i class='icon-exclamation-sign'></i> " + error);
+    }
+  };
+
+  coffeescriptOptions = {
+    mode: 'coffeescript',
+    tabMode: 'indent',
+    tabSize: 2,
+    onKeyEvent: function(editor, keyEvent) {
+      return compile(editor, $.event.fix(keyEvent));
+    }
+  };
+
+  htmlOptions = {
+    mode: 'html',
+    tabMode: 'indent',
+    tabSize: 2,
+    lineNumbers: true
+  };
 
   $(document).ready(function() {
-    var coffeescriptOptions, htmlOptions;
-    coffeescriptOptions = {
-      mode: 'coffeescript',
-      tabMode: 'indent',
-      tabSize: 2
-    };
-    htmlOptions = {
-      mode: 'html',
-      tabMode: 'indent',
-      tabSize: 2,
-      lineNumbers: true
-    };
     $("textarea.coffeescript").each(function() {
-      return CodeMirror.fromTextArea($(this)[0], coffeescriptOptions);
+      var editor;
+      return editor = CodeMirror.fromTextArea($(this)[0], coffeescriptOptions);
     });
     return $("textarea.html").each(function() {
       return CodeMirror.fromTextArea($(this)[0], htmlOptions);
--- src/app.coffee
+++ src/app.coffee
@@ -1,16 +1,33 @@
-$(document).ready ->
-  coffeescriptOptions = {
-    mode: 'coffeescript'
-    tabMode: 'indent'
-    tabSize: 2
-  }
-  htmlOptions = {
-    mode: 'html'
-    tabMode: 'indent'
-    tabSize: 2
-    lineNumbers: true
-  }
+compile = (editor, keyEvent) ->
+  return unless keyEvent.type is "keyup"
+  return if keyEvent.keyCode in [16,37,38,39,40] # ignore arrow and shift keys
   
-  $("textarea.coffeescript").each -> CodeMirror.fromTextArea($(this)[0], coffeescriptOptions)
+  alertDiv = $(editor.getTextArea()).data('alert')
+  source = editor.getValue()
+  try
+    javascript = CoffeeScript.compile(source, bare: on)
+    eval javascript
+    $(alertDiv).html "<i class='icon-ok'></i> Successfully Compiled"
+  catch error
+    $(alertDiv).html "<i class='icon-exclamation-sign'></i> #{error}"
 
-  $("textarea.html").each -> CodeMirror.fromTextArea($(this)[0], htmlOptions)
+coffeescriptOptions = {
+  mode: 'coffeescript'
+  tabMode: 'indent'
+  tabSize: 2
+  onKeyEvent: (editor, keyEvent) -> compile(editor, $.event.fix(keyEvent))
+}
+
+htmlOptions = {
+  mode: 'html'
+  tabMode: 'indent'
+  tabSize: 2
+  lineNumbers: true
+}
+
+$(document).ready ->
+  $("textarea.coffeescript").each ->
+    editor = CodeMirror.fromTextArea($(this)[0], coffeescriptOptions)
+
+  $("textarea.html").each ->
+    CodeMirror.fromTextArea($(this)[0], htmlOptions)
